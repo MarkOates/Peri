ea87eba98ef7dc526161148dcb8f4f7abbdacbeb
Mark Oates <markoates0@gmail.com>
2021-08-07 12:33:55 -0400

    * Test error message on missing display for Peri/TitleScreen

--- tests/Peri/TitleScreenTest.cpp
+++ tests/Peri/TitleScreenTest.cpp
@@ -24,11 +24,11 @@ TEST_F(Peri_TitleScreenTestWithEmptyFixture, can_be_created_without_blowing_up)
    Peri::TitleScreen title_screen;
 }
 
-TEST_F(Peri_TitleScreenTestWithEmptyFixture, render__without_a_screen__raises_an_exception)
+TEST_F(Peri_TitleScreenTestWithEmptyFixture, primary_timer_func__without_a_display__raises_an_exception)
 {
-   //Peri::BoardRenderer board_renderer;
-   //std::string expected_error_message = "BoardRenderer::render: error: guard \"board\" not met";
-   //ASSERT_THROW_WITH_MESSAGE(board_renderer.render(), std::runtime_error, expected_error_message);
+   Peri::TitleScreen title_screen;
+   std::string expected_error_message = "TitleScreen::primary_timer_func: error: guard \"display\" not met";
+   ASSERT_THROW_WITH_MESSAGE(title_screen.primary_timer_func(), std::runtime_error, expected_error_message);
 }
 
 TEST_F(Peri_TitleScreenTestWithAllegroRenderingFixture, render__renders_the_board)
