72fae5c608db1127685713835678fd867916b568
Mark Oates <markoates0@gmail.com>
2021-08-07 12:35:51 -0400

    * Add initial Testing/WithAllegroFlareFrameworkFixture

---
+++ include/Testing/WithAllegroFlareFrameworkFixture.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+
+#include <AllegroFlare/FontBin.hpp>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
+#include <allegro_flare/placement2d.h>
+#include <allegro_flare/placement3d.h>
+#include <gtest/gtest.h>
+
+
+namespace Testing
+{
+   class WithAllegroFlareFrameworkFixture : public ::testing::Test
+   {
+   private:
+      ALLEGRO_DISPLAY* display;
+      AllegroFlare::FontBin font_bin;
+
+   public:
+      WithAllegroFlareFrameworkFixture();
+      virtual ~WithAllegroFlareFrameworkFixture();
+
+      ALLEGRO_DISPLAY* get_display();
+      AllegroFlare::FontBin &get_font_bin_ref();
+      virtual void SetUp() override;
+      virtual void TearDown() override;
+      ALLEGRO_FONT* get_any_font();
+      allegro_flare::placement3d build_centered_placement3d(float width=0.0f, float height=0.0f);
+      allegro_flare::placement2d build_centered_placement2d(float width=0.0f, float height=0.0f);
+   };
+}
+
+
+
---
+++ src/Testing/WithAllegroFlareFrameworkFixture.cpp
@@ -0,0 +1,97 @@
+
+
+#include <Testing/WithAllegroFlareFrameworkFixture.hpp>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_primitives.h>
+#include <stdexcept>
+#include <sstream>
+#include <stdexcept>
+#include <sstream>
+
+
+namespace Testing
+{
+
+
+WithAllegroFlareFrameworkFixture::WithAllegroFlareFrameworkFixture()
+   : ::testing::Test()
+   , display(nullptr)
+   , font_bin({})
+{
+}
+
+
+WithAllegroFlareFrameworkFixture::~WithAllegroFlareFrameworkFixture()
+{
+}
+
+
+ALLEGRO_DISPLAY* WithAllegroFlareFrameworkFixture::get_display()
+{
+   return display;
+}
+
+
+AllegroFlare::FontBin &WithAllegroFlareFrameworkFixture::get_font_bin_ref()
+{
+   return font_bin;
+}
+
+
+void WithAllegroFlareFrameworkFixture::SetUp()
+{
+   ASSERT_EQ(false, al_is_system_installed());
+   ASSERT_EQ(true, al_init());
+   al_init_primitives_addon();
+   al_init_font_addon();
+   al_init_ttf_addon();
+
+   font_bin.set_full_path("/Users/markoates/Repos/hexagon/bin/programs/data/fonts");
+
+   display = al_create_display(1280 * 2, 720 * 2);
+   al_clear_to_color(ALLEGRO_COLOR{0.05f, 0.05f, 0.05f, 0.05f});
+   return;
+}
+
+void WithAllegroFlareFrameworkFixture::TearDown()
+{
+   font_bin.clear();
+   al_destroy_display(display);
+   al_shutdown_ttf_addon(); // this is required otherwise subsequent al_init_ttf_addon will not work
+                            // this is a bug in Allegro
+   al_uninstall_system();
+   return;
+}
+
+ALLEGRO_FONT* WithAllegroFlareFrameworkFixture::get_any_font()
+{
+   return font_bin.auto_get("consolas.ttf 32");
+}
+
+allegro_flare::placement3d WithAllegroFlareFrameworkFixture::build_centered_placement3d(float width, float height)
+{
+   if (!(al_get_target_bitmap()))
+      {
+         std::stringstream error_message;
+         error_message << "WithAllegroFlareFrameworkFixture" << "::" << "build_centered_placement3d" << ": error: " << "guard \"al_get_target_bitmap()\" not met";
+         throw std::runtime_error(error_message.str());
+      }
+   allegro_flare::placement3d place(al_get_display_width(display)/2, al_get_display_height(display)/2, 0.0f);
+   place.size = AllegroFlare::vec3d(width, height, 0);
+   return place;
+}
+
+allegro_flare::placement2d WithAllegroFlareFrameworkFixture::build_centered_placement2d(float width, float height)
+{
+   if (!(al_get_target_bitmap()))
+      {
+         std::stringstream error_message;
+         error_message << "WithAllegroFlareFrameworkFixture" << "::" << "build_centered_placement2d" << ": error: " << "guard \"al_get_target_bitmap()\" not met";
+         throw std::runtime_error(error_message.str());
+      }
+   allegro_flare::placement2d place(al_get_display_width(display)/2, al_get_display_height(display)/2, width, height);
+   return place;
+}
+} // namespace Testing
+
+
